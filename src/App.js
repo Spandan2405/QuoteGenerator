db031b47f1d068fd3b7431a43eff9f20dfa254d9
Spandan Gupta <spandan2405@gmail.com>
2024-01-28 13:35:36 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { advice: '', backgroundImage: '' };
+
+  componentDidMount() {
+    this.fetchAdvice();
+    this.fetchRandomImage();
+  }
+
+  fetchAdvice = () => {
+    axios.get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  fetchRandomImage = () => {
+    axios.get("https://source.unsplash.com/random/1920x1080/?nature")
+      .then((response) => {
+        const backgroundImage = response.request.responseURL;
+        this.setState({ backgroundImage });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { advice, backgroundImage } = this.state;
+    const backgroundStyle = backgroundImage ? { backgroundImage: `url(${backgroundImage})` } : {};
+
+    return (
+      <div className='app' style={backgroundStyle}>
+        <div className='card'>
+          <h1 className='heading'>{advice}</h1>
+          <button className='button' onClick={() => { this.fetchAdvice(); this.fetchRandomImage(); }}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
